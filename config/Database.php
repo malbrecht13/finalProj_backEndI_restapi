2b19f5f8bcad090d8ef8e6891b1725f4a3a11ce3
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-04-30 05:30:36 -0500

    try localhost

--- config/Database.php
+++ config/Database.php
@@ -2,22 +2,19 @@
 
 class Database {
     //DB params
+    private $host = 'localhost';
+    private $db_name = 'quotesdb';
+    private $username = 'root';
+    private $password = '';
     private $conn;
 
     //DB connect
     public function connect() {
         $this->conn = null;
-        $url = getenv('JAWSDB_URL');
-        $dbparts = parse_url($url);
-        $hostname = $dbparts['host'];
-        $username = $dbparts['user'];
-        $password = $dbparts['pass'];
-        $database = ltrim($dbparts['path'],'/');
-
-        $dsn = "mysql:host={$hostname};dbname={$database}";
 
         try {
-            $this->conn = new PDO($dsn, $username, $password);
+            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
+            $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
