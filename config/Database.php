6643ec1713f8638d2b4296eb8984c8a069cbd1d0
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-04-30 05:34:45 -0500

    try visible info

--- config/Database.php
+++ config/Database.php
@@ -2,10 +2,10 @@
 
 class Database {
     //DB params
-    private $host = 'localhost';
-    private $db_name = 'quotesdb';
-    private $username = 'root';
-    private $password = '';
+    private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    private $db_name = 'a1kn963erd2y2x2z';
+    private $username = 'p2ravqd64g5faddc';
+    private $password = 'd0ka9lbq678i7xla';
     private $conn;
 
     //DB connect
@@ -21,6 +21,27 @@ class Database {
         }
         return $this->conn;
     }
+
+    //DB connect
+    // public function connect() {
+    //     $this->conn = null;
+    //     $url = getenv('JAWSDB_URL');
+    //     $dbparts = parse_url($url);
+    //     $hostname = $dbparts['host'];
+    //     $username = $dbparts['user'];
+    //     $password = $dbparts['pass'];
+    //     $database = ltrim($dbparts['path'],'/');
+
+    //     $dsn = "mysql:host={$hostname};dbname={$database}";
+
+    //     try {
+    //         $this->conn = new PDO($dsn, $username, $password);
+    //         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     } catch (PDOException $e) {
+    //         echo 'Connection Error: ' . $e->getMessage();
+    //     }
+    //     return $this->conn;
+    // }
 }
     
 
