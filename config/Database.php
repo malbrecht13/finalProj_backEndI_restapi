ae4f28679711b83705b5559b91538b225963d569
malbrecht13 <44516083+malbrecht13@users.noreply.github.com>
2021-04-30 05:50:40 -0500

    Try again with jasdb env variables

--- config/Database.php
+++ config/Database.php
@@ -2,46 +2,28 @@
 
 class Database {
     //DB params
-    private $host = 'frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-    private $db_name = 'a1kn963erd2y2x2z';
-    private $username = 'p2ravqd64g5faddc';
-    private $password = 'd0ka9lbq678i7xla';
     private $conn;
 
     //DB connect
     public function connect() {
         $this->conn = null;
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
+        $hostname = $dbparts['host'];
+        $username = $dbparts['user'];
+        $password = $dbparts['pass'];
+        $database = ltrim($dbparts['path'],'/');
+
+        $dsn = "mysql:host={$hostname};dbname={$database}";
 
         try {
-            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, 
-            $this->username, $this->password);
+            $this->conn = new PDO($dsn, $username, $password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
         }
         return $this->conn;
     }
-
-    //DB connect
-    // public function connect() {
-    //     $this->conn = null;
-    //     $url = getenv('JAWSDB_URL');
-    //     $dbparts = parse_url($url);
-    //     $hostname = $dbparts['host'];
-    //     $username = $dbparts['user'];
-    //     $password = $dbparts['pass'];
-    //     $database = ltrim($dbparts['path'],'/');
-
-    //     $dsn = "mysql:host={$hostname};dbname={$database}";
-
-    //     try {
-    //         $this->conn = new PDO($dsn, $username, $password);
-    //         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //     } catch (PDOException $e) {
-    //         echo 'Connection Error: ' . $e->getMessage();
-    //     }
-    //     return $this->conn;
-    // }
 }
     
 
